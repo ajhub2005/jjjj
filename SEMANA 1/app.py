ff706046368ee12e501edf3ac799b7f597a6839b
ajhub2005 <106087363+ajhub2005@users.noreply.github.com>
2022-10-14 06:51:28 -0500

    Add files via upload

---
+++ SEMANA 1/app.py
@@ -0,0 +1,45 @@
+#escribimos 4 onzas de queso
+
+'''
+comment 1
+comment 2
+comment 3
+'''
+'''
+
+print("identify your name")
+name=input()
+#print("you name is",name)
+print("you identification is {name}")
+'''
+'''
+print("how are you,put your name. ")
+name=input()
+print("good job,insert your last name.")
+apellido=input()
+print(f"welcome {name} {apellido}")
+'''
+
+'''print("numero total de aprendices")
+numero2=int(input())
+print("numero total de niñas")
+numero3=int(input())
+resultado= numero3/numero2*100
+
+print(f" bien, el resultado es {resultado}")'''
+
+
+
+
+#let's calculate the age
+print("please enter the current year")
+year1=int(input())
+print("please type your year of birth")
+year2=int(input())
+age= year1-year2
+print(f"this is his age, {age}")
+
+
+
+
+
